80b10233722ad3902778cd9ff8c345ebd8f760df
drakesobe <drake.sobe@gmail.com>
2025-08-21 22:35:03 -0400

    Remove SpeedInsights completely

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,6 @@
 // pages/_app.js
 import '../styles/globals.css';
-import SpeedInsights from '@vercel/speed-insights'; // correct import
 
 export default function MyApp({ Component, pageProps }) {
-  // run SpeedInsights only on the server
-  if (typeof window === "undefined") {
-    SpeedInsights();
-  }
-
   return <Component {...pageProps} />;
 }

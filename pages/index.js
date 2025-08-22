6c02186d662b520749ed3ad6a6f919b886e3dc95
drakesobe <drake.sobe@gmail.com>
2025-08-21 22:06:41 -0400

    Remove front-end SpeedInsights usage

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import { useState } from "react";
 import NavBar from "../components/NavBar";
 import SearchBar from "../components/SearchBar";
 import OCRResults from "../components/OCRResults";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function HomePage() {
   const [activeTab, setActiveTab] = useState("Search");

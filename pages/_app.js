8d05abd718cbd3ac06c647289f328720c25082e8
drakesobe <drake.sobe@gmail.com>
2025-09-09 16:40:52 -0400

    updated app

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@
 import "@/styles/globals.css";
 import { AuthProvider } from "@/hooks/useAuth";
 import { Toaster } from "react-hot-toast";
-import Head from "next/head";
 import Script from "next/script";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
@@ -12,7 +11,7 @@ export default function MyApp({ Component, pageProps }) {
 
   useEffect(() => {
     const handleRouteChange = (url) => {
-      if (typeof window.gtag !== "undefined") {
+      if (typeof window.gtag === "function") {
         window.gtag("config", "G-0HXXN1SJ9K", { page_path: url });
       }
     };
@@ -21,47 +20,28 @@ export default function MyApp({ Component, pageProps }) {
   }, [router.events]);
 
   return (
-    <AuthProvider>
-      <Head>
-        <title>CheckPeak</title>
-
-        {/* Main favicon */}
-        <link rel="icon" href="/favicon.ico" />
-
-        {/* PNG favicons */}
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
-
-        {/* Apple/iOS icon */}
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-
-        {/* Web App Manifest */}
-        <link rel="manifest" href="/site.webmanifest" />
-
-        {/* RealFaviconGenerator icons */}
-        <link rel="icon" type="image/png" sizes="192x192" href="/web-app-manifest-192x192.png" />
-        <link rel="icon" type="image/png" sizes="512x512" href="/web-app-manifest-512x512.png" />
-
-        {/* Theme color for mobile browsers */}
-        <meta name="theme-color" content="#000000" />
-      </Head>
-
-      {/* Google Analytics */}
+    <>
+      {/* 1️⃣ Load GA script */}
       <Script
-        strategy="afterInteractive"
         src="https://www.googletagmanager.com/gtag/js?id=G-0HXXN1SJ9K"
+        strategy="afterInteractive"
       />
+
+      {/* 2️⃣ Initialize GA */}
       <Script id="gtag-init" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'G-0HXXN1SJ9K', { page_path: window.location.pathname });
+          gtag('config', 'G-0HXXN1SJ9K');
         `}
       </Script>
 
-      <Component {...pageProps} />
-      <Toaster position="top-right" />
-    </AuthProvider>
+      {/* 3️⃣ Your app */}
+      <AuthProvider>
+        <Component {...pageProps} />
+        <Toaster position="top-right" />
+      </AuthProvider>
+    </>
   );
 }
